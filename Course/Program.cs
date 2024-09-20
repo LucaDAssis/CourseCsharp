51c60ea1dea748f3e32c0726bfb46f54f89befcf
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-09-20 11:52:57 -0300

    Operadores de atribuicao

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,32 +5,24 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Bom Dia!!");
-        Console.Write("Boa Noite");
-        Console.Write("Bom Dia!!");
+        int a = 10;
+        Console.WriteLine(a);
 
-        char gener = 'A';
-        int idade = 10;
-        double saldo = 100.449494949;
-        String nome = "Luscas";
+        a += 2;
+        Console.WriteLine(a);
 
-        Console.WriteLine("------------------------------------");
+        a++;
+        Console.WriteLine(a);
 
-        Console.WriteLine(nome);
-        Console.WriteLine(idade);
-        Console.WriteLine(saldo.ToString("F2"));
-        Console.WriteLine(saldo.ToString("F4"));
-        Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
-        Console.WriteLine(gener);
+        a--;
+        Console.WriteLine(a);
 
-        //placehoder
-        Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:f2} reais", nome, idade, saldo);
 
-        //placehoder melhor
-        Console.WriteLine($"{nome} tem {idade} anos e tem saldo igal a {saldo:F2} reais");
+        int b = a++;
+        Console.WriteLine(b);
 
-        //Concatenação
-        Console.WriteLine(nome + "tem " + idade + "anos e tem saldo igual a " + saldo + "reais");
+        int c = ++a;
+        Console.WriteLine(c);
 
     }
 }
