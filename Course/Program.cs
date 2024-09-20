572e8b74761b78d1ca9ed9ed6f983a8011358187
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-09-20 11:11:20 -0300

    Adicionar arquivos de projeto.

---
+++ Course/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SByte x = 100;
+
+        Console.WriteLine(x);
+
+        byte n1 = 1;
+
+        Console.WriteLine(n1);
+        
+    }
+}
