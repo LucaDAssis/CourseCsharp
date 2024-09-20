7bd3e0a9f8f975c8201b491e65cec987ad954efb
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-09-20 14:19:18 -0300

    conversao implicita e casting

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,30 +5,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string produto1 = "Computador";
-        string produto2 = "Mesa de escritório";
-        byte idade = 30;
-        int codigo = 5290;
-        char genero = 'M';
-        double preco1 = 2100.0;
-        double preco2 = 650.50;
-        double medida = 53.234567;
+        double a;
+        float b;
 
-        Console.WriteLine("Produtos: ");
-        Console.WriteLine($"{produto1}, Cujo o preço é ${preco1}");
-        Console.WriteLine($"{produto2}, cujo o preço é ${preco2}");
+        a = 5.1;
+        b = (float)a;
 
-        Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero{genero}");
+        Console.WriteLine(b);
 
-        // Exibir medida com oito casas decimais
-        Console.WriteLine($"Medida com oito casas decimais: {medida:F8}");
+        int c = 5;
+        int y = 2;
 
-        // Arredondar para três casas decimais e exibir
-        Console.WriteLine($"Arredondado (três casas decimais): {Math.Round(medida, 3)}");
+        double resultado = (double) c / y;
 
-        // Exibir medida com três casas decimais usando CultureInfo.InvariantCulture
-        Console.WriteLine(medida.ToString("F3", CultureInfo.InvariantCulture));
-    
+        Console.WriteLine(resultado);
 
     }
 }
