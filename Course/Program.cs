6f7f879ea2a487805d0a92d7a832b65b231dc4ed
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-09-20 11:37:57 -0300

    saidas de dados em csharp

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,41 +1,36 @@
-﻿internal class Program
+﻿using System.Globalization;
+using System;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        SByte x = 100;
+        Console.WriteLine("Bom Dia!!");
+        Console.Write("Boa Noite");
+        Console.Write("Bom Dia!!");
 
-        Console.WriteLine(x);
+        char gener = 'A';
+        int idade = 10;
+        double saldo = 100.449494949;
+        String nome = "Luscas";
 
-        byte n = 1;
+        Console.WriteLine("------------------------------------");
 
-        Console.WriteLine(n);
-
-        bool completo = false;
-        char genero = 'F';
-        char letra = '\u0041';
-        byte n1 = 126;
-        int n2 = 1000;
-        int n3 = 2147483647;
-        long n4 = 2147483648L;
-        float n5 = 4.5f;
-        double n6 = 4.5;
-        String nome = "Maria Green";
-        Object obj1 = "Alex Brown";
-        Object obj2 = 4.5f;
-
-        Console.WriteLine(completo);
-        Console.WriteLine(genero);
-        Console.WriteLine(letra);
-        Console.WriteLine(n1);
-        Console.WriteLine(n2);
-        Console.WriteLine(n3);
-        Console.WriteLine(n4);
-        Console.WriteLine(n5);
-        Console.WriteLine(n6);
         Console.WriteLine(nome);
-        Console.WriteLine(obj1);
-        Console.WriteLine(obj2);
+        Console.WriteLine(idade);
+        Console.WriteLine(saldo.ToString("F2"));
+        Console.WriteLine(saldo.ToString("F4"));
+        Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+        Console.WriteLine(gener);
 
+        //placehoder
+        Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:f2} reais", nome, idade, saldo);
+
+        //placehoder melhor
+        Console.WriteLine($"{nome} tem {idade} anos e tem saldo igal a {saldo:F2} reais");
+
+        //Concatenação
+        Console.WriteLine(nome + "tem " + idade + "anos e tem saldo igual a " + saldo + "reais");
 
     }
 }
