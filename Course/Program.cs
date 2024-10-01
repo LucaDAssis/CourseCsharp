3d9e22f6ee56a4a025475210176f1e690ece3461
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-10-01 14:40:01 -0300

    Membros estáticos

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,31 +5,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Produto p = new Produto();
-        Console.WriteLine("Entre os dados do produto:");
-        Console.Write("Nome: ");
-        p.Nome = Console.ReadLine();
-        Console.Write("Preço: ");
-        p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-        Console.Write("Quantidade no estoque: ");
-        p.Quantidade = int.Parse(Console.ReadLine());
-        Console.WriteLine();
-        Console.WriteLine("Dados do produto: " + p);
-        Console.WriteLine();
-        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-        int qte = int.Parse(Console.ReadLine());
-        p.AdicionarProdutos(qte);
-        Console.WriteLine();
-        Console.WriteLine("Dados atualizados: " + p);
-        Console.WriteLine();
-        Console.Write("Digite o número de produtos a ser removido do estoque: ");
-        qte = int.Parse(Console.ReadLine());
-        p.RemoverProdutos(qte);
-        Console.WriteLine();
-        Console.WriteLine("Dados atualizados: " + p);
-
-
-
+        Console.Write("Entre o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double circ = Calculadora.Circunferencia(raio);
+        double volume = Calculadora.Volume(raio);
+        Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+        Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2",
+        CultureInfo.InvariantCulture));
 
     }
 }
