d706a04a233510a9920413ad86567aeeb8e221c6
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-10-01 14:22:26 -0300

    Produtos

--- Course/Program.cs
+++ Course/Program.cs
@@ -4,42 +4,30 @@ using System.Globalization;
 internal class Program
 {
     private static void Main(string[] args)
-    {  
+    {
+        Produto p = new Produto();
+        Console.WriteLine("Entre os dados do produto:");
+        Console.Write("Nome: ");
+        p.Nome = Console.ReadLine();
+        Console.Write("Preço: ");
+        p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade no estoque: ");
+        p.Quantidade = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.WriteLine("Dados do produto: " + p);
+        Console.WriteLine();
+        Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+        int qte = int.Parse(Console.ReadLine());
+        p.AdicionarProdutos(qte);
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + p);
+        Console.WriteLine();
+        Console.Write("Digite o número de produtos a ser removido do estoque: ");
+        qte = int.Parse(Console.ReadLine());
+        p.RemoverProdutos(qte);
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: " + p);
 
-        Pessoas p1 = new Pessoas();
-        Pessoas p2 = new Pessoas();
-        Pessoas p3 = new Pessoas();
-
-        Console.WriteLine("Dados da Primeira Pessoa: ");
-        p1.nome = Console.ReadLine();
-        p1.idade = int.Parse(Console.ReadLine());
-        Console.WriteLine("Dados da Segunda Pessoa: ");
-        p2.nome = Console.ReadLine();
-        p2.idade = int.Parse(Console.ReadLine());
-
-        if (p1.idade > p2.idade)
-        {
-            Console.WriteLine(p1.nome);
-        }
-        else
-        {
-            Console.WriteLine(p2.nome);
-        }
-
-
-        Funcionario f1 = new Funcionario();
-        Funcionario f2 = new Funcionario();
-
-        Console.WriteLine("Dados do primeiro funcionário: ");
-        f1.nome = Console.ReadLine(); 
-        f1.salario = double.Parse(Console.ReadLine());
-        Console.WriteLine("Dados do Segundo Funcionário: ");
-        f2.nome = Console.ReadLine();
-        f2.salario = double.Parse(Console.ReadLine());
-
-        double medio = (f1.salario + f2.salario) / 2;
-
-        Console.WriteLine(medio);
 
 
 
