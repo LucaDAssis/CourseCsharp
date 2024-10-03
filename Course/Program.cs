004dc4ca4d0fcd964b4d94656dab5ff082d881ab
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-10-03 14:38:15 -0300

    Propriedades e Operações com DateTime

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,23 +5,20 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        DateTime dateTime = DateTime.Now;
-        Console.WriteLine(dateTime);
-
-        DateTime data = DateTime.Now;
-        DateTime data1 = DateTime.Today;
-        DateTime d1 = DateTime.Parse("07/12/1994");
-
-        Console.WriteLine(data);
-        Console.WriteLine(data1);
-        Console.WriteLine(d1);
-
-        TimeSpan t1 = new TimeSpan();
-        TimeSpan t2 = new TimeSpan(9000000L);
-        TimeSpan t3 = new TimeSpan(2, 11, 21);
-
-        Console.WriteLine(t1);
-        Console.WriteLine(t3);
-        Console.WriteLine(t2);
+        DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+        Console.WriteLine(d);
+        Console.WriteLine("1) Date: " + d.Date);
+        Console.WriteLine("2) Day: " + d.Day);
+        Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+        Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+        Console.WriteLine("5) Hour: " + d.Hour);
+        Console.WriteLine("6) Kind: " + d.Kind);
+        Console.WriteLine("7) Millisecond: " + d.Millisecond);
+        Console.WriteLine("8) Minute: " + d.Minute);
+        Console.WriteLine("9) Month: " + d.Month);
+        Console.WriteLine("10) Second: " + d.Second);
+        Console.WriteLine("11) Ticks: " + d.Ticks);
+        Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+        Console.WriteLine("13) Year: " + d.Year);
     }
 }
