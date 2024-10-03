dca885899839b5aab6989756effd89fe39e61690
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-10-03 10:46:20 -0300

    List com varios exemploss

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,12 +5,44 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        String[] vect = new String[] { "MAria", "Bob", "J" };
-
-        foreach (String obj in vect)
+        List<string> list = new List<string>();
+        list.Add("Maria");
+        list.Add("Alex");
+        list.Add("Bob");
+        list.Add("Anna");
+        list.Insert(2, "Marco");
+        foreach (string obj in list)
         {
             Console.WriteLine(obj);
         }
+        Console.WriteLine("List count: " + list.Count);
+        string s1 = list.Find(x => x[0] == 'A');
+        Console.WriteLine("First 'A': " + s1);
+        string s2 = list.FindLast(x => x[0] == 'A');
+        Console.WriteLine("Last 'A': " + s2);
+        int pos1 = list.FindIndex(x => x[0] == 'A');
+        Console.WriteLine("First position 'A': " + pos1);
+        int pos2 = list.FindLastIndex(x => x[0] == 'A');
+        Console.WriteLine("Last position 'A': " + pos2);
+        List<string> list2 = list.FindAll(x => x.Length == 5);
+        Console.WriteLine("---------------------");
+        foreach (string obj in list2)
+        {
+            Console.WriteLine(obj);
+        }
+        list.Remove("Alex");
+        Console.WriteLine("---------------------");
+        foreach (string obj in list)
+        {
+            Console.WriteLine(obj);
+        }
+        list.RemoveAll(x => x[0] == 'M');
+        Console.WriteLine("---------------------");
+        foreach (string obj in list)
+        {
+            Console.WriteLine(obj);
+        }
+
 
 
     }
