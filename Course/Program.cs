3bab4adb9599bd7e30139c329f70d89f38897554
LucaDAssis <lucasrafaeisaac@gmail.com>
2024-10-03 11:03:12 -0300

    exercicio employee

--- Course/Program.cs
+++ Course/Program.cs
@@ -5,45 +5,46 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        List<string> list = new List<string>();
-        list.Add("Maria");
-        list.Add("Alex");
-        list.Add("Bob");
-        list.Add("Anna");
-        list.Insert(2, "Marco");
-        foreach (string obj in list)
+        Console.Write("How many employees will be registered? ");
+        int n = int.Parse(Console.ReadLine());
+
+        List<Employee> list = new List<Employee>();
+
+        for (int i = 1; i <= n; i++)
         {
-            Console.WriteLine(obj);
-        }
-        Console.WriteLine("List count: " + list.Count);
-        string s1 = list.Find(x => x[0] == 'A');
-        Console.WriteLine("First 'A': " + s1);
-        string s2 = list.FindLast(x => x[0] == 'A');
-        Console.WriteLine("Last 'A': " + s2);
-        int pos1 = list.FindIndex(x => x[0] == 'A');
-        Console.WriteLine("First position 'A': " + pos1);
-        int pos2 = list.FindLastIndex(x => x[0] == 'A');
-        Console.WriteLine("Last position 'A': " + pos2);
-        List<string> list2 = list.FindAll(x => x.Length == 5);
-        Console.WriteLine("---------------------");
-        foreach (string obj in list2)
-        {
-            Console.WriteLine(obj);
-        }
-        list.Remove("Alex");
-        Console.WriteLine("---------------------");
-        foreach (string obj in list)
-        {
-            Console.WriteLine(obj);
-        }
-        list.RemoveAll(x => x[0] == 'M');
-        Console.WriteLine("---------------------");
-        foreach (string obj in list)
-        {
-            Console.WriteLine(obj);
+            Console.WriteLine("Employee #" + i + ":");
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            list.Add(new Employee(id, name, salary));
+            Console.WriteLine();
         }
 
+        Console.Write("Enter the employee id that will have salary increase : ");
+        int searchId = int.Parse(Console.ReadLine());
 
+        Employee emp = list.Find(x => x.Id == searchId);
+        if (emp != null)
+        {
+            Console.Write("Enter the percentage: ");
+            double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            emp.IncreaseSalary(percentage);
+        }
+        else
+        {
+            Console.WriteLine("This id does not exist!");
+        }
 
+        Console.WriteLine();
+        Console.WriteLine("Updated list of employees:");
+        foreach (Employee obj in list)
+        {
+            Console.WriteLine(obj);
+        }
     }
+
+
 }
